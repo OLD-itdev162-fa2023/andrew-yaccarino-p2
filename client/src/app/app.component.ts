1eda97b7db38a5628105e072cdb5eb13618a086d
AndrewYacc <78662462+AndrewYacc@users.noreply.github.com>
2022-10-19 13:14:12 -0500

    Completed Activity 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,17 +6,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
-  weatherForecasts: any;
+  posts: any;
   
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.http.get("http://localhost:5235/weatherforecast").subscribe(
-      response => { this.weatherForecasts = response; },
+    this.http.get("http://localhost:5235/api/posts").subscribe(
+      response => { this.posts = response; },
       error => { console.log(error); }
     );
   }
